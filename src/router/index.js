616f0f24459e728ff530461c3fe10433beb29840
Mambou Jorel <linkadaair@gmail.com>
2024-01-31 14:19:16 +0100

    ajout de dashbord1view dans le dossie vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginViewVue from '@/views/LoginView.vue'
+import dashbord1ViewVue from '@/views/dashbord1View.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
     {
       path: '/login',
       name: 'login',
       component: LoginViewVue
+    },
+    {
+      path: '/dashbord1',
+      name: 'dashbord1',
+      component: dashbord1ViewVue
     },
     {
       path: '/about',
